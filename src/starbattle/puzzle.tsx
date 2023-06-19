5ba7fd3e4a815c9923805a343db615584d79a00b
David Tan <dtan@scopemedia.com>
2023-06-19 15:50:58 -0700

    Implement walls

--- src/starbattle/puzzle.tsx
+++ src/starbattle/puzzle.tsx
@@ -1,11 +1,12 @@
 import { useMemo, useState } from "react";
+import classNames from 'classnames';
 
 import './puzzle.css'
 
 enum Cell {
     BLANK,
+    STAR,
     SPACE,
-    STAR
 }
 
 function makeColumnView(cells: Cell[], size: number): Cell[][] {
@@ -14,7 +15,7 @@ function makeColumnView(cells: Cell[], size: number): Cell[][] {
         const column: Cell[] = []
         columns.push(column)
         for (let y = 0; y < size; y++) {
-            const i = y * size + x
+            const i = getIndex(x, y, size)
             column.push(cells[i])
         }
     }
@@ -30,7 +31,7 @@ function makeRowView(columns: Cell[][]): Cell[][] {
     return rows
 }
 
-function makeGroupView(cells: Cell[], walls: boolean[]): Cell[][] {
+function makeGroupView(cells: Cell[], horizontalWalls: boolean[], verticalWalls: boolean[]): Cell[][] {
     //todo
     return []
 }
@@ -60,34 +61,29 @@ function makeNeighbourView(cells: Cell[], size: number): Cell[][] {
 
 type CellProps = {
     value: Cell
+    className: string
     onClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
-function StarBattleCell({ value, onClick }: CellProps): JSX.Element {
+function StarBattleCell({ value, className, onClick }: CellProps): JSX.Element {
     const typeToSymbol = {
         [Cell.BLANK]: "",
-        [Cell.SPACE]: "✖",
         [Cell.STAR]: "★",
+        [Cell.SPACE]: "✖",
     }
     const symbol = typeToSymbol[value]
-    return <button className="StarBattle-Cell" onClick={onClick}>{symbol}</button>
+    return <button className={className} onClick={onClick}>{symbol}</button>
 }
 
 export default function StarBattlePuzzle({ size = 10, starCount = 2 }): JSX.Element {
-    const [cells, setCells] = useState(Array(size**2).fill(Cell.STAR))
-    const [walls, setWalls] = useState(Array((size-1)**2).fill(false))
+    const [cells, setCells] = useState(Array(size**2).fill(Cell.BLANK))
+    const [horizontalWalls, setHorizontalWalls] = useState(Array(size*(size-1)).fill(false))
+    const [verticalWalls, setVerticalWalls] = useState(Array(size*(size-1)).fill(false))
     const columns = useMemo(() => makeColumnView(cells, size), [cells, size])
     const rows = useMemo(() => makeRowView(columns), [columns])
     const neighbours = useMemo(() => makeNeighbourView(cells, size), [cells, size])
-    const groups = useMemo(() => makeGroupView(cells, walls), [cells, walls])
-
-    function getCellType(x: number, y: number): Cell | undefined {
-        if (x < 0 || x >= size || y < 0 || y >= size) {
-            return
-        }
-        return columns[x][y]
-    }
-
+    const groups = useMemo(() => makeGroupView(cells, horizontalWalls, verticalWalls), [cells, horizontalWalls, verticalWalls])
+    
     /**
      * isSolvable - Returns true if puzzle has no mistakes
      */
@@ -123,8 +119,9 @@ export default function StarBattlePuzzle({ size = 10, starCount = 2 }): JSX.Elem
         return true
     }
 
-    function getStarCount(cells: Cell[]) {
-        return cells.reduce((sum, current) => current == Cell.STAR ? sum + 1 : sum, 0)
+    function getCell(x: number, y: number): Cell | undefined {
+        if (x < 0 || x >= size || y < 0 || y >= size) return
+        return columns[x][y]
     }
 
     function setCell(i: number, newValue: Cell) {
@@ -133,8 +130,83 @@ export default function StarBattlePuzzle({ size = 10, starCount = 2 }): JSX.Elem
         setCells(newCells)
     }
 
+    // horizontal walls are in size columns, size-1 rows
+    function horizontalWallExists(x: number, y: number): boolean  {
+        if (x < 0 || x >= size || y < 0 || y >= size - 1) return false
+        return horizontalWalls[getIndex(x, y, size)]
+    }
+
+    function toggleHorizontalWall(x: number, y: number) {
+        if (x < 0 || x >= size || y < 0 || y >= size - 1) return
+        const i = getIndex(x, y, size)
+        const newWalls = horizontalWalls.slice()
+        newWalls[i] = !newWalls[i]
+        setHorizontalWalls(newWalls)
+    }
+
+    // vertical walls are in size-1 columns, size rows
+    function verticalWallExists(x: number, y: number): boolean {
+        if (x < 0 || x >= size - 1 || y < 0 || y >= size) return false
+        return verticalWalls[getIndex(x, y, size - 1)]
+    }
+
+    function toggleVerticalWall(x: number, y: number) {
+        if (x < 0 || x >= size - 1 || y < 0 || y >= size) return
+        const i = getIndex(x, y, size - 1)
+        const newWalls = verticalWalls.slice()
+        newWalls[i] = !newWalls[i]
+        setVerticalWalls(newWalls)
+    }
+
+    function makeCellClickHandler(i: number) {
+        const { x, y } = getCoords(i, size)
+        const margin = 7 // px
+        return (event: React.MouseEvent<HTMLElement>) => {
+            if (event.target instanceof HTMLElement) {
+                const { clientX, clientY, target: { offsetLeft, offsetTop, offsetWidth, offsetHeight }} = event
+                console.log(x, y, clientX, clientY, offsetLeft, offsetTop, offsetLeft + offsetWidth, offsetTop + offsetHeight)
+                if (Math.abs(offsetLeft - clientX) < margin) return toggleVerticalWall(x - 1, y)
+                if (Math.abs(offsetTop - clientY) < margin) return toggleHorizontalWall(x, y - 1)
+                if (Math.abs(offsetLeft + offsetWidth - clientX) < margin) return toggleVerticalWall(x, y)
+                if (Math.abs(offsetTop + offsetHeight - clientY) < margin) return toggleHorizontalWall(x, y)
+                setCell(i, (cells[i] + 1) % 3)
+            }
+        }
+    }
+
+    function makeCellClassNames(i: number) {
+        const { x, y } = getCoords(i, size)
+        return classNames({
+            'StarBattle-Cell': true,
+            'StarBattle-Cell-Border-Left': verticalWallExists(x - 1, y),
+            'StarBattle-Cell-Border-Top': horizontalWallExists(x, y - 1),
+            'StarBattle-Cell-Border-Right': verticalWallExists(x, y),
+            'StarBattle-Cell-Border-Bottom': horizontalWallExists(x, y),
+        })
+    }
+
     const contentCells = cells.map((cell, i) => (
-        <StarBattleCell key={i} value={cell} onClick={() => setCell(i, (cells[i] + 1) % 3)}/>
+        <StarBattleCell
+            key={i}
+            value={cell}
+            className={makeCellClassNames(i)}
+            onClick={makeCellClickHandler(i)}
+        />
     ))
     return <div className="StarBattle-Puzzle">{contentCells}</div>
+}
+
+function getStarCount(cells: Cell[]) {
+    return cells.reduce((sum, current) => current == Cell.STAR ? sum + 1 : sum, 0)
+}
+
+function getCoords(i: number, size: number) {
+    return {
+        x: i % size,
+        y: Math.floor(i / size),
+    }
+}
+
+function getIndex(x: number, y: number, size: number) {
+    return y * size + x
 }

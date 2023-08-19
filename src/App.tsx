e47c1795b4adbbbff0ddfde9f6d01e1e347384d5
David Tan <dtan@scopemedia.com>
2023-08-18 19:08:39 -0700

    Use MUI dark theme and add CSS baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import logo from './logo.svg';
 import './App.css';
 
 import StarBattlePuzzle from './starbattle/puzzle';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <StarBattlePuzzle />
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <StarBattlePuzzle />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
